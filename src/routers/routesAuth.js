5f000d0073e1e0b82415218e981519b090a60988
Nguyen Tuan <norsinlight@gmail.com>
2024-03-30 01:41:08 +0700

    add server and handle forgot password

--- src/routers/routesAuth.js
+++ src/routers/routesAuth.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Register from "pages/authentication/Register";
 import Login from "pages/authentication/Login";
+import ResetPassword from "pages/authentication/ResetPassword";
 
 var routesAuth = [
     {
@@ -15,6 +16,13 @@ var routesAuth = [
         name: "Register",
         icon: "ni ni-circle-08 text-pink",
         component: <Register />,
+        layout: "/auth",
+    },
+    {
+        path: "/change-password",
+        name: "Register",
+        icon: "ni ni-circle-08 text-pink",
+        component: <ResetPassword />,
         layout: "/auth",
     },
 ];
